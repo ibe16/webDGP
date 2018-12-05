7d69b693958e9c8d4f30e2801d29bdab35f998e5
Jacob <jacobfolchsola@gmail.com>
2018-12-05 18:26:51 +0100

    Añadido funcionalidad de la vista añadir ruta

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-/* Requeriments */
+/* Requerimientos */
 const express = require('express');
 const ejs = require('ejs');
-
-/* firebase */
+var bodyParser = require('body-parser');
 var firebase = require('firebase');
 var admin = require('firebase-admin');
+
+
+
+
+/* FireBase Configuration */
 var serviceAccount = require('./credenciales.json');
-
-
-/* Configuration */
 var testfire = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://granaroutesaplicacion.firebaseio.com'
 });
-
-console.log(testfire.name);
+console.log('[*]Base de Datos '+testfire.name+' inicializada.');
 // Acceso data - Ejemplo
 // var db = admin.database();
 // var ref = db.ref("/");
@@ -27,6 +27,8 @@ const app = express();
 
 /* APP configuration */
 app.use(express.static(__dirname + '/public')); // If it doesn't find resoruces, GOTO public
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.set('view engine', 'ejs');
 
 /* Render pages */
@@ -40,19 +42,14 @@ app.get('/login', (req, res) => res.render('login'));
 // register
 app.get('/register', (req, res) => res.render('registrarse'));
 
-// listado Rutas
-//app.get('/list', (req, res) => res.render('listaRutas'));
-
 //Consulta a la BD para caragar todas las rutas
 app.get('/listaRutas', function (req, res, next) {
   var db = admin.database();
   var ref = db.ref("rutas");
   //Se cargan las rutas
-  res.render('listaRutas', { ref: ref});
+  res.render('listaRutas', {ref: ref});
 
 });
-
-
 
 // proponerRutas
 app.get('/newRoute', (req, res) => res.render('proponerRuta'));
@@ -74,13 +71,83 @@ app.get('/gestor', (req, res) => res.render('gestor'));
 // Sugerencias
 app.get('/listaProposiciones', (req, res) => res.render('listaProposiciones'));
 
-// Crear Ruta
-//app.get('/crearRuta', (req, res) => res.render('crearRuta'));
-
+// Crear Ruta - Formulario
 app.get('/crearRuta', function(req, res, next){
   var db = admin.database();
   var ref = db.ref("lugares");
-  res.render('crearRuta', {ref :ref});
+  res.render('crearRuta', { ref : ref });
+});
+
+// Crear Ruta - Subirla a FireBase
+app.post('/crearRuta', (req, res) => {
+  /* Variables Auxiliares */
+  var f_para = req.body;
+  var f_valida = true;
+  var nueva_ruta = {}
+
+  /* Comprueba que estan todos los parametros || TODO : Comprobar formato INPUT */
+  // Titulo
+  if(!f_para.hasOwnProperty('titulo') || f_para.titulo.length <= 0) {
+    f_valida = false;
+  }
+  // Descripcion
+  if(!f_para.hasOwnProperty('descripcion') || f_para.titulo.length <= 0) {
+    f_valida = false;
+  }
+  // Url's
+  if(!f_para.hasOwnProperty('mapaAndroid') ||
+     !f_para.hasOwnProperty('mapaWEB') ||
+     f_para.mapaAndroid.length <= 0 ||
+     f_para.mapaWEB.length <= 0) {
+    f_valida = false;
+  }
+  // Lugares
+  if(!f_para.hasOwnProperty('lugares')) {
+    f_valida = false;
+  }
+  // Grupos
+  if(!f_para.hasOwnProperty('grupos')){
+    f_valida = false;
+  }
+
+  if(!f_valida) {
+    // Si el formato no es valido saca mensaje
+    // TODO : Render pagina anterior con campos erroneos resaltados en rojo
+    res.send('Por favor vuelva a rellenar el formulario y \
+    no deje ningún campo en blanco. <a href="/crearRuta">Atrás</a>');
+  }
+
+  /* Genera el Obj para subirlo a FireBase */
+  // Objeto básico
+  nueva_ruta[f_para.titulo] = {
+    descripcion : f_para.descripcion,
+    map : f_para.mapaAndroid,
+    mapWeb : f_para.mapaWEB,
+    nombre : f_para.titulo
+  }
+  // Añadir los grupos || Razón -> Añadir booleano al value de la key
+  nueva_ruta[f_para.titulo].grupos = {};
+  for (const key in f_para.grupos) {
+    if (f_para.grupos.hasOwnProperty(key)) {
+      nueva_ruta[f_para.titulo].grupos[key] = true;
+    }
+  }
+  // Añadir los lugares || Razón -> Pasar de Array a Objeto y añadir booleano
+  nueva_ruta[f_para.titulo].lugares = {};
+  f_para.lugares.forEach(element => {
+    nueva_ruta[f_para.titulo].lugares[element] = true;
+  });
+
+  /* Subir la nueva ruta a FireBase */
+  // Coje instancia de FireBase y colocate en rutas
+  var rutasRef = admin.database().ref('rutas');
+
+  // Sube la nueva Ruta
+  rutasRef.update(nueva_ruta);
+  
+  // Mensaje todo ha salido bien
+  res.send('Ruta añadida correctamente! Siga el <a href="/gestor">enlace</a> para volver al menu.');
+
 });
 
 //crearLugar
@@ -98,5 +165,5 @@ app.use(function(req, res, next) {
 });
 
 /* Start the server */
-app.listen(8008);
-console.log('Express server listening on port 8008.');
+app.listen(8080);
+console.log('Express server listening on port 8080.');
